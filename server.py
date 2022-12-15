381becad285a51143f20a6741ca17488e2b46709
Skoh <101289702+SkohTV@users.noreply.github.com>
2022-12-15 23:19:31 +0100

    Flask now works

--- server.py
+++ server.py
@@ -1,10 +1,33 @@
-from flask import Flask
-
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/")
+def accueil():
+	return render_template("accueil.html")
+@app.route('/accueil')
+def accueilR1():
+    return redirect("/")
 
-@app.route("/home/")  # this sets the route to this page
-def home():
-	return "Hello! this is the main page <h1>HELLO</h1>"  # some basic inline html
+@app.route('/histoire')
+def histoire():
+    return render_template("histoire.html")
+
+@app.route('/lieux')
+def lieux():
+    return render_template("lieux.html")
+
+@app.route('/lieux/1')
+def lieu1():
+    return render_template("lieux/1.html")
+@app.route('/lieux/2')
+def lieu2():
+    return render_template("lieux/2.html")
+@app.route('/lieux/3')
+def lieu3():
+    return render_template("lieux/3.html")
+@app.route('/lieux/4')
+def lieu4():
+    return render_template("lieux/4.html")
+
+if __name__ == "__main__":
+    app.run(debug=True ,port=8080,use_reloader=False)
